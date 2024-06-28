912382dba05e53fab767e30018f9e5be7458a0ee
Leon Lan <l.lan@vu.nl>
2024-06-28 10:48:09 +0200

    BKS in VRPLIB style
    10 seeds, 2.4*n time limit, AMD EPYC 7H12

---
+++ bks/X148-HVRP.sol
@@ -0,0 +1,56 @@
+Route #1: 135 100
+Route #2: 16 131 102
+Route #3: 47
+Route #4: 115
+Route #5: 71 145
+Route #6: 53 124
+Route #7: 114 31 19
+Route #8: 69 63 24
+Route #9: 55 22
+Route #10: 75
+Route #11: 99
+Route #12: 117 15
+Route #13: 57 7 98
+Route #14: 5
+Route #15: 118
+Route #16: 119 48 6
+Route #17: 44
+Route #18: 104 136
+Route #19: 54 10 68
+Route #20: 110 137
+Route #21: 36 66 13 33
+Route #22: 46 23
+Route #23: 34 49
+Route #24: 73
+Route #25: 32 82
+Route #26: 25 29
+Route #27: 3 12
+Route #28: 45 51
+Route #29: 140 121
+Route #30: 50 9 11 38
+Route #31: 86 27 111
+Route #32: 65 144
+Route #33: 39 89 126
+Route #34: 26 130
+Route #35: 122 84
+Route #36: 59 1
+Route #37: 106 129
+Route #38: 14 61 67
+Route #39: 96 91 139 79 92
+Route #40: 88 18 42
+Route #41: 109 17 112 2
+Route #42: 128 146 123
+Route #43: 35 70 113 142
+Route #44: 52 40 105 37 8
+Route #45: 143 132 56
+Route #46: 60 64 94 138 4
+Route #47: 30 97 28
+Route #48: 108 74 90 116 125
+Route #49: 62 85 127 77 134 107
+Route #50: 20 81 120 80 103 101
+Route #51: 43 76 95 87 83
+Route #52: 21 93 72 58
+Route #53: 41 78 133 141 147
+Route #54: 
+Route #55: 
+Cost: 80367.02
---
+++ bks/X219-HD.sol
@@ -0,0 +1,86 @@
+Route #1: 5 176
+Route #2: 185 93
+Route #3: 59 198
+Route #4: 20 41
+Route #5: 64 51
+Route #6: 36 76
+Route #7: 122 27
+Route #8: 110 107
+Route #9: 193 124
+Route #10: 146 30
+Route #11: 157 87
+Route #12: 112 3
+Route #13: 16 167
+Route #14: 70 12
+Route #15: 39 32
+Route #16: 7 48
+Route #17: 4 191
+Route #18: 9 61
+Route #19: 190 71
+Route #20: 92 168
+Route #21: 26 192
+Route #22: 129 158
+Route #23: 166
+Route #24: 180 173
+Route #25: 130 133
+Route #26: 95 52
+Route #27: 
+Route #28: 
+Route #29: 
+Route #30: 
+Route #31: 
+Route #32: 
+Route #33: 
+Route #34: 
+Route #35: 208 1 97
+Route #36: 2 65 72
+Route #37: 188 132 62
+Route #38: 108 179 189
+Route #39: 113 204 81
+Route #40: 75 46 114
+Route #41: 170 123 47
+Route #42: 131 138 183
+Route #43: 155 181 178
+Route #44: 125 139 210
+Route #45: 22 57 209
+Route #46: 91 89 211
+Route #47: 6 202 214
+Route #48: 164 127 115
+Route #49: 80 14 11
+Route #50: 83 116 88
+Route #51: 25 53 128
+Route #52: 194 120 196
+Route #53: 67 35 106
+Route #54: 13 218 56
+Route #55: 55 54 151
+Route #56: 85 162 147
+Route #57: 172 165 153
+Route #58: 206 60 140
+Route #59: 74 137 205
+Route #60: 207 134 201
+Route #61: 177 77 149
+Route #62: 136 69 212
+Route #63: 82 98 141
+Route #64: 24 49 117
+Route #65: 99 78 18
+Route #66: 10 45 111
+Route #67: 156 58 171
+Route #68: 
+Route #69: 100 145 186 143
+Route #70: 184 213 19 187
+Route #71: 148 200 33 144
+Route #72: 142 50 103 216
+Route #73: 37 175 159 102
+Route #74: 163 101 86 197
+Route #75: 96 152 31 217
+Route #76: 15 38 40 150
+Route #77: 118 23 29 154
+Route #78: 43 169 161 34
+Route #79: 63 42 44 203
+Route #80: 119 215 8 21
+Route #81: 109 84 160 66
+Route #82: 199 182 28 195
+Route #83: 105 68 94 126
+Route #84: 17 73 174 104
+Route #85: 90 135 79 121
+Cost: 120986.4
